8face45e76a7ee1d8527e99e29bd95fe1a417037
Varada Rajendrakumar Kulkarni <kulkarni.va@northeastern.edu>
2021-11-28 18:49:35 -0500

    initial commit

---
+++ whatsapp-clone/src/mockData.js
@@ -0,0 +1,88 @@
+export const contactList = [
+  {
+    id: 1,
+    name: "Varada Kulkarni",
+    profilePic: "/whatsapp-clone/profile/varada.jpeg",
+    lastText: "Bye",
+    lastTextTime: "12:58 PM",
+  },
+  {
+    id: 2,
+    name: "Nisha Mishra",
+    profilePic: "/whatsapp-clone/profile/nisha.jpeg",
+    lastText: `What are you doing?`,
+    lastTextTime: "1:45 PM",
+  },
+  {
+    id: 3,
+    name: "Riti Gawade",
+    profilePic: "/whatsapp-clone/profile/riti.jpeg",
+    lastText: "Where are you?",
+    lastTextTime: "12:30 AM",
+  },
+  {
+    id: 4,
+    name: "Akshay Khandelwal",
+    profilePic: "/whatsapp-clone/profile/akshay.jpeg",
+    lastText: "I m at the library",
+    lastTextTime: "12:00 PM",
+  },
+  {
+    id: 5,
+    name: "Jatin Sharma",
+    profilePic: "/whatsapp-clone/profile/jatin.jpeg",
+    lastText: "Good Night",
+    lastTextTime: "8:23 AM",
+  },
+  {
+    id: 6,
+    name: "Manthan Wadiwala",
+    profilePic: "/whatsapp-clone/profile/manthan.jpeg",
+    lastText: "Could you help me with the Assignment?",
+    lastTextTime: "9:18 PM",
+  }
+];
+export const messagesList = [
+  {
+    id: 1,
+    messageType: "TEXT",
+    text: "Hey! 😁",
+    senderID: 0,
+    addedOn: "12:00 PM",
+  },
+  {
+    id: 2,
+    messageType: "TEXT",
+    text: "Hey, What's up?",
+    senderID: 1,
+    addedOn: "12:01 PM",
+  },
+  {
+    id: 3,
+    messageType: "TEXT",
+    text: "All Good, What about you?",
+    senderID: 0,
+    addedOn: "12:00 PM",
+  },
+  {
+    id: 4,
+    messageType: "TEXT",
+    text: "I'm good as well",
+    senderID: 1,
+    addedOn: "12:00 PM",
+  },
+  {
+    id: 5,
+    messageType: "TEXT",
+    text: "Great 😁",
+    senderID: 0,
+    addedOn: "12:00 PM",
+  },
+  {
+    id: 5,
+    messageType: "TEXT",
+    text: "Are you done with your project?",
+    senderID: 1,
+    addedOn: "12:00 PM",
+  },
+];
